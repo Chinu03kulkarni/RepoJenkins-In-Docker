4ed62c394aed26c44c6ae718889c14f346cb270b
Chinu03kulkarni <chinmayeek03@gmail.com>
2022-01-09 12:37:48 +0530

    Added java demo application files

---
+++ src/main/java/org/example/Java_jenkis_in_docker/App.java
@@ -0,0 +1,13 @@
+package org.example.Java_jenkis_in_docker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/example/Java_jenkis_in_docker/Main.java
@@ -0,0 +1,17 @@
+package org.example.Java_jenkis_in_docker;
+
+
+
+class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(checkIfInputIsAnEvenNumber(122));
+	}
+	
+
+public static boolean checkIfInputIsAnEvenNumber(int
+		number){
+		return number % 2 == 0;
+		}
+}
