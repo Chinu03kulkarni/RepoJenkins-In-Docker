d509be876ae8d45aac1c9799c4c2499944157a2a
Chinu03kulkarni <chinmayeek03@gmail.com>
2022-01-10 18:48:59 +0530

    modify changes

--- src/main/java/org/example/Java_jenkis_in_docker/TestMain.java
+++ src/main/java/org/example/Java_jenkis_in_docker/TestMain.java
@@ -1,12 +1,13 @@
 package org.example.Java_jenkis_in_docker;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TestMain {
 
-	
-		@Test
-		public void testInputIsEven(){
-		assertTrue(Main.checkIfInputIsAnEvenNumber(22)); //Assertion
+	@Test
+	public void testInputIsEven()
+	{
+		assertTrue(Main.checkIfInputIsAnEvenNumber(23));
 	}
-
 }
